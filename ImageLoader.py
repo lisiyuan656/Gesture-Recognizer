003362a8618c394164e9fb8d42f533e27e25c541
reid-fu <fu.349@osu.edu>
2016-11-18 15:56:12 -0500

    wrote function to load images from folder

--- ImageLoader.py
+++ ImageLoader.py
@@ -1,2 +1,11 @@
 from scipy import misc
+import os
 
+class ImageLoader():
+    def loadImages(self, partNum):
+        images = []
+        directory = 'data/part' + str(partNum)
+        for img in os.listdir():
+            image = misc.imread(directory + img)
+            images.append(image)
+        return images
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import numpy
 import scipy
+from ImageLoader import ImageLoader
 
+imgLoader = ImageLoader()
+part1 = imgLoader.loadImages(1)
+print part1
