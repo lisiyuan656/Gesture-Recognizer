ca7112a621087d500105f3238d663386bc8c4d3d
reid-fu <fu.349@osu.edu>
2016-11-18 15:00:18 -0500

    added tutorials folder and some preliminary files

---
+++ ImageLoader.py
@@ -0,0 +1,2 @@
+from scipy import misc
+
---
+++ tutorials/load_image.py
@@ -0,0 +1,5 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+
+test = misc.imread('../data/part1/hand1_0_bot_seg_1_cropped.png')
+plt.show(test)
