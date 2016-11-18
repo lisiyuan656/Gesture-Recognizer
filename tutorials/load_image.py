59333be3c8ac45ee4ac4a4fe7e8177c21bab1297
reid-fu <fu.349@osu.edu>
2016-11-18 15:17:26 -0500

    finished image loading tutorial

--- tutorials/load_image.py
+++ tutorials/load_image.py
@@ -1,5 +1,8 @@
+import os
 from scipy import misc
 import matplotlib.pyplot as plt
 
+print os.path.isfile("../data/part1/hand1_0_bot_seg_1_cropped.png")
 test = misc.imread('../data/part1/hand1_0_bot_seg_1_cropped.png')
-plt.show(test)
+plt.imshow(test)
+plt.show()
