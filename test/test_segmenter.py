1bb03a4a1dcea82e2ecfa8889d148a4014e80ce7
reid-fu <fu.349@osu.edu>
2016-11-23 17:20:57 -0500

    Added test folder

---
+++ test/test_segmenter.py
@@ -0,0 +1,8 @@
+from scipy import misc
+from img_loading import ImageLoader
+from preprocessing import ImgSegmenter
+
+img = ImageLoader().loadImage('../data/part1/hand1_0_bot_seg_1_cropped.png')
+segmenter = ImgSegmenter()
+binImg = segmenter.binarizeImg(img)
+misc.imsave('binImg.png', binImg)
