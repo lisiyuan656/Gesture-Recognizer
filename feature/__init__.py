51bbbe7bc0ca065bfa91c18fced65897fa949abc
reid-fu <fu.349@osu.edu>
2016-11-27 21:20:31 -0500

    created global singleton objects in init scripts

---
+++ feature/__init__.py
@@ -0,0 +1,6 @@
+from pca import PCA
+from Interests import Interest_points
+
+NUM_EVECTS_PCA = 2
+pc_analyzer = PCA(NUM_EVECTS_PCA)
+interest_pt_analyzer = Interest_points()
--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,3 +1,5 @@
 from preprocessing.img_segmenting import ImgSegmenter
+from preprocessing import NoiseRemoval
 
+noise_remover = NoiseRemoval()
 segmenter = ImgSegmenter(5)
