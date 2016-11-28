150b0e5b7bbb4811af370db74a7b442d2566b0c7
Siyuan <lisiyuan656@gmail.com>
2016-11-28 18:26:18 -0500

    Initial develop

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from preprocessing.NoiseRemoval import NoiseRemoval
 from preprocessing.img_segmenting import ImgSegmenter
 from preprocessing.imageScaler import imageScaler
 from preprocessing.process_data import process_data
+import numpy as np
+from feature.pca import PCA
 
 data = ImageLoader().getData(5)
 data_size = len(data)
@@ -14,34 +16,38 @@ binData = ImgSegmenter(5).binarizeSet(data) # segment the images
 random.shuffle(data)
 train_data_size = 2000
 training_data = data[1:train_data_size+1]
-training_x, training_y = process_data().process_data(training_data)
-testing_data = data[train_data_size+1:]
-
 
 
 # Calculate eigenvectors
-eigenvectors = numpy.zeros((36,15,590))
+mean_eigenvectors = np.zeros((36,15,590))
+mean_eigenvectors = calculate_mean(training_data)
+""" input is the training_data, formated the same as data
+    output should be a ndarray of dim (36, 590, basisDim)
+    code below doesn't work, but should be helpful
+"""
+"""
 for i in range(10):
     curchar = chr(i+48)
-    temp = numpy.zeros((590,15))
+    temp = np.zeros((590,15))
     count = 0
-    for tuple in data:
+    for tuple in training_data:
         if tuple[1]==curchar:
             temp = temp + PCA(tuple[0])
             count = count + 1
     if count!=0:
         temp = temp / count
     eigenvectors[i]=temp
-
-
 for i in range(26):
     curchar = chr(i+97)
-    temp = numpy.zeros((590,15))
+    temp = np.zeros((590,15))
     count = 0
-    for tuple in data:
+    for tuple in training_data:
         if tuple[1]==curchar:
             temp = temp + PCA(tuple[0])
             count = count + 1
     if count!=0:
         temp = temp / count
     eigenvectors[i+10]=temp
+"""
+training_x, training_y = process_data().process_data(training_data)
+testing_data = data[train_data_size+1:]
