b868794002c1b0f08597e3ef33a47be3a8eb3770
reid-fu <fu.349@osu.edu>
2016-11-29 15:55:26 -0500

    commit before merge

--- main.py
+++ main.py
@@ -2,48 +2,19 @@ import random
 from img_loading import ImageLoader
 from preprocessing import noise_remover, img_scaler, segmenter
 from preprocessing.process_data import process_data
-import numpy as np
 from feature import pc_analyzer
 
+# Load and preprocess data
 data = ImageLoader().getData(5)
 data_size = len(data)
 data = img_scaler.scaleDataset(data) # rescale the images
 data = noise_remover.Gaussian_filter(data, 3) # blur the images
 binData = segmenter.binarizeSet(data) # segment the images
-
+# Shuffle data and split into training and test
 random.shuffle(data)
 train_data_size = 2000
-training_data = data[1:train_data_size+1]
-
-# Calculate eigenvectors
-mean_eigenvectors = pc_analyzer.calculate_mean(training_data)
-""" input is the training_data, formated the same as data
-    output should be a ndarray of dim (36, 590, basisDim)
-    code below doesn't work, but should be helpful
-"""
-"""
-for i in range(10):
-    curchar = chr(i+48)
-    temp = np.zeros((590,15))
-    count = 0
-    for tuple in training_data:
-        if tuple[1]==curchar:
-            temp = temp + PCA(tuple[0])
-            count = count + 1
-    if count!=0:
-        temp = temp / count
-    eigenvectors[i]=temp
-for i in range(26):
-    curchar = chr(i+97)
-    temp = np.zeros((590,15))
-    count = 0
-    for tuple in training_data:
-        if tuple[1]==curchar:
-            temp = temp + PCA(tuple[0])
-            count = count + 1
-    if count!=0:
-        temp = temp / count
-    eigenvectors[i+10]=temp
-"""
-training_x, training_y = process_data().process_data(training_data)
-testing_data = data[train_data_size+1:]
+train_data = data[0:train_data_size]
+testing_data = data[train_data_size:]
+# Initialize features in training set
+mean_eigenvectors = pc_analyzer.calculate_mean(train_data)
+train_x, train_y = process_data().process_data(train_data)
--- preprocessing/process_data.py
+++ preprocessing/process_data.py
@@ -3,11 +3,11 @@ import process_img
 import process_label
 
 def process_data(dataset, basisDim, mean_eigenvectors):
-    training_x = np.array([])
-    training_y = np.array([])
+    train_x = np.array([])
+    train_y = np.array([])
     for datapoint in dataset:
         img = datapoint[0]
         label = datapoint[1]
-        training_x = np.append(training_x, process_img(img, basisDim, mean_eigenvectors))
-        training_y = np.append(training_y, process_label(label))
-    return training_x, training_y
+        train_x = np.append(train_x, process_img(img, basisDim, mean_eigenvectors))
+        train_y = np.append(train_y, process_label(label))
+    return train_x, train_y
