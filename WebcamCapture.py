7d543876563ea0793b7aad579317f0589f8f52fa
Unknown <zjtxljyus@gmail.com>
2016-11-30 17:52:49 -0500

    Update WebcamCap

--- WebcamCapture.py
+++ WebcamCapture.py
@@ -16,11 +16,18 @@ else:
     rval = False
 
 while rval:
-    cv2.rectangle(frame,(590,660),(1,1),(0,255,0),0)
+    cv2.rectangle(frame,(295,330),(1,1),(0,255,0),0)
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 32: # exit on ESC
+        image = frame[1:331,1:296]
+        imageresized = cv2.resize(image,(590,660),interpolation = cv2.INTER_CUBIC)
+        grayimage = color.rgb2gray(imageresized)*255
+        image = noise_remover.Gaussian_filter_forImg(grayimage,3)
+        binimage = segmenter.backgroundSubtract(image)
+        
+        cv2.imshow("preview",imageresized)
+    if key == 27:
         break
 vc.release()
-cv2.destroyWindow("preview")
