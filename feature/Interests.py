92e92ff741aeac76bfa2d0f356356cc44be2c353
Siyuan <lisiyuan656@gmail.com>
2016-12-01 01:01:20 -0500

    Best one

--- feature/Interests.py
+++ feature/Interests.py
@@ -7,3 +7,23 @@ class Interest_points():
         kp = fast.detect(img, None)
         #print "Total Keypoints with nonmaxSuppression", len(kp)
         return len(kp)
+
+    def get_min_max_distance(self, img, num_minmax):
+        fast = cv2.FastFeatureDetector()
+        fast.setBool('nonmaxSuppression',0)
+        kp = fast.detect(img, None)
+        M = cv2.moments(img)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        distance = []
+        for i in range(len(kp)):
+            distance.append(np.linalg.norm(np.asarray(kp[i].pt) - np.asarray((cx,cy))))
+        distance.sort()
+        res = np.array([])
+        if (len(kp)==0):
+            return np.zeros(num_minmax*2)
+        for i in range(num_minmax):
+            res = np.append(res, distance[i%len(kp)])
+        for i in range(num_minmax):
+            res = np.append(res, distance[(len(kp)-1-i)%len(kp)])
+        return res
--- preprocessing/process_img.py
+++ preprocessing/process_img.py
@@ -7,11 +7,13 @@ from feature.Interests import Interest_points
 
 def process_img(img, basisDim, data_mean_evecs):
     res = np.array([])
+    num_minmax = 5
     eigenvecs = PCA(basisDim).getEigVecs(img)
     for i in range(basisDim):
         for j in range(36):
             dis = np.linalg.norm(eigenvecs[:,i]-data_mean_evecs[j,:,i])
             res = np.append(res, dis)
     res = np.append(res, np.asarray(MomentsCalculator().ImageMoments(img)))
+    #res = np.append(res, Interest_points().get_min_max_distance(img, num_minmax))
     res = np.append(res, Interest_points().fast_feature(img))
     return res
