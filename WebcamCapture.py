978edb3777b95bc6706a003088bc5c7c9de6dfc9
Unknown <zjtxljyus@gmail.com>
2016-12-01 02:30:29 -0500

    Update CamCapture

--- WebcamCapture.py
+++ WebcamCapture.py
@@ -5,13 +5,19 @@ Created on Wed Nov 30 12:23:01 2016
 @author: Jiyuan
 """
 
+import numpy as np
 import cv2
-from preprocessing import noise_remover, segmenter
+import pickle
+from learning.knn_wrapper import KNNClassifier
+from preprocessing import noise_remover
 from skimage import color
-from preprocessing.process_img import process_img
+from feature.MomentsCalculator import MomentsCalculator
 
 cv2.namedWindow("preview")
 cv2.namedWindow("MOG")
+pkl_file = open('knn.pkl','rb')
+knn = pickle.load(pkl_file)
+pkl_file.close()
 vc = cv2.VideoCapture(0)
 fgbg = cv2.BackgroundSubtractorMOG(200,5,0.7)
 
@@ -19,24 +25,24 @@ if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
+    
+str = "Hello World!"
 
 while rval:
     cv2.rectangle(frame,(295,330),(1,1),(0,255,0),0)
-    cv2.putText(frame, "Hello World!", (400,400), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255))
+    cv2.putText(frame, str, (400,400), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255))
     cv2.imshow("preview", frame)
+    rval, frame = vc.read()
     fgmask = fgbg.apply(frame)
     cv2.imshow('MOG',fgmask)
-    rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 32: # exit on ESC
-        image = frame[1:331,1:296]
+        image = fgmask[1:331,1:296]
         imageresized = cv2.resize(image,(590,660),interpolation = cv2.INTER_CUBIC)
-        grayimage = color.rgb2gray(imageresized)*255
-        image = noise_remover.Gaussian_filter_forImg(grayimage,3)
-        binimage = segmenter.backgroundSubtract(image)
-        input_x = process_img(binimage)
-        #put input_x into trained nl or something else
-        cv2.imshow("preview",imageresized)
+        image = noise_remover.Gaussian_filter_forImg(imageresized,3)
+        input_x = np.asarray(MomentsCalculator().ImageMoments(image))
+        output = knn.predict(input_x)
+        str = output[0]
     if key == 27:
         break
 vc.release()
--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import string
 from util.img_loading import ImageLoader
 from preprocessing import noise_remover, img_scaler, segmenter
 from preprocessing.process_data import process_data
+from preprocessing.resize_wrapper import resize_wrapper
 from feature import pc_analyzer
 # from learning.neural_net_wrapper import NeuralNet
 from learning.knn_wrapper import KNNClassifier
 import numpy
 
 # Load and preprocess data
-data = ImageLoader().getData(1)
+data = ImageLoader().getData(5)
 data_size = len(data)
 basisDim = 15
 data = img_scaler.scaleDataset(data) # rescale the images
+#data = resize_wrapper(data,4)
 data = noise_remover.Gaussian_filter(data, 3) # blur the images
-binData = segmenter.binarizeSet(data) # segment the images
+data = segmenter.binarizeSet(data) # segment the images
 # Shuffle data and split into training and test
 random.shuffle(data)
-train_data_size = data_size/4*3
+train_data_size = data_size
 train_data = data[0:train_data_size]
 test_data = data[train_data_size:]
 # Initialize features in training set
@@ -30,10 +32,10 @@ train_y = train_y.reshape(train_data_size, 36)
 # Learn to recognize gestures
 knn = KNNClassifier()
 knn.train(train_x, [train_data[i][1] for i in range(0,train_data_size)])
-test_x, test_y = process_data(test_data, basisDim, mean_eigenvectors)
-test_x = test_x.reshape(data_size/4,36*basisDim+7+1)
-test_y = [test_data[i][1] for i in range(0,data_size/4)]
-output = knn.predict(test_x)
+#test_x, test_y = process_data(test_data, basisDim, mean_eigenvectors)
+#test_x = test_x.reshape(data_size/4,36*basisDim+7+1)
+#test_y = [test_data[i][1] for i in range(0,data_size/4)]
+#output = knn.predict(test_x)
 # input_size = 36*basisDim + 7 + 1
 # nn = NeuralNet([input_size,30,36])
 # error = nn.train(train_x, train_y, 5, 0.05)
