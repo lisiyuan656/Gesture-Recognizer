e79626607ce646b2b81c90576ff152b01b438d77
Siyuan <lisiyuan656@gmail.com>
2016-11-30 23:33:40 -0500

    Test neural nets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import string
 from util.img_loading import ImageLoader
 from preprocessing import noise_remover, img_scaler, segmenter
 from preprocessing.process_data import process_data
+from preprocessing.resize_wrapper import resize_wrapper
 from feature import pc_analyzer
 # from learning.neural_net_wrapper import NeuralNet
 from learning.nn_pybrain import nn_pybrain
@@ -14,29 +15,29 @@ from pybrain.utilities           import percentError
 # Load and preprocess data
 data = ImageLoader().getData(1)
 data_size = len(data)
-basisDim = 15
+basisDim = 10
 data = img_scaler.scaleDataset(data) # rescale the images
+data = resize_wrapper(data, 4)
 data = noise_remover.Gaussian_filter(data, 3) # blur the images
 data = segmenter.binarizeSet(data) # segment the images
+
 # Shuffle data and split into training and test
 random.shuffle(data)
 train_data = data
 train_data_size = data_size
 print "Finish preprocessing..."
-#train_data = data[0:train_data_size]
-#test_data = data[train_data_size:]
-# Initialize features in training set
-"""
+
 category_order = [str(i) for i in range(0,10)] + list(string.ascii_lowercase)
 mean_eigenvectors = pc_analyzer.calculate_mean(train_data, category_order)
 mean_eigenvectors = numpy.asarray(mean_eigenvectors)
-"""
-train_x, train_y = process_data(train_data)
-train_x = train_x.reshape(train_data_size, 36*basisDim+7+1)
-train_x = train_x[:, 36*basisDim:]
+
+
+train_x, train_y = process_data(train_data, basisDim, mean_eigenvectors)
+train_x = train_x.reshape(train_data_size, 36*basisDim+8)
+#train_x = train_x[:, 36*basisDim:]
 train_y = train_y.reshape(train_data_size, 1)
-#input_size = 36*basisDim + 7 + 1
-input_size = 8
+input_size = 36*basisDim + 7 + 1
+#input_size = img_size
 output_size = 36
 train_dataset_pybrain = ClassificationDataSet(input_size, nb_classes=36, class_labels=category_order)
 train_dataset_pybrain.setField('input', train_x)
@@ -52,10 +53,10 @@ for n in xrange(0, trndata_temp.getLength()):
 trndata._convertToOneOfMany()
 tstdata._convertToOneOfMany()
 # Classify using feedforward neural nets
-nn = nn_pybrain([input_size, 15, output_size])
+nn = nn_pybrain([input_size, output_size])
 nn.enroll(trndata)
 print "Begin training neural nets..."
-epochs = 200
+epochs = 400
 for i in range(epochs):
     nn.train_for_one_epoch()
     trnresult = percentError(nn.trainer.testOnClassData(), trndata['class'])
--- preprocessing/process_data.py
+++ preprocessing/process_data.py
@@ -2,13 +2,13 @@ import numpy as np
 import process_img
 import process_label
 
-def process_data(dataset):
+def process_data(dataset, basisDim, mean_eigenvectors):
     train_x = np.array([[]])
     train_y = np.array([])
     for datapoint in dataset:
         img = datapoint[0]
         label = datapoint[1]
-        #train_x = np.append(train_x, process_img.process_img(img, basisDim, mean_eigenvectors))
-        train_x = np.append(train_x, img.reshape(1, img.size))
+        train_x = np.append(train_x, process_img.process_img(img, basisDim, mean_eigenvectors))
+        #train_x = np.append(train_x, img.reshape(1, img.size))
         train_y = np.append(train_y, process_label.process_label(label))
     return train_x, train_y
