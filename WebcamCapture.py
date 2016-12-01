1543b91bee06ad1d64fec6efbe48aced37f49d60
Unknown <zjtxljyus@gmail.com>
2016-12-01 01:32:45 -0500

    Update camera capture

--- WebcamCapture.py
+++ WebcamCapture.py
@@ -6,9 +6,14 @@ Created on Wed Nov 30 12:23:01 2016
 """
 
 import cv2
+from preprocessing import noise_remover, segmenter
+from skimage import color
+from preprocessing.process_img import process_img
 
 cv2.namedWindow("preview")
+cv2.namedWindow("MOG")
 vc = cv2.VideoCapture(0)
+fgbg = cv2.BackgroundSubtractorMOG(200,5,0.7)
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -17,7 +22,10 @@ else:
 
 while rval:
     cv2.rectangle(frame,(295,330),(1,1),(0,255,0),0)
+    cv2.putText(frame, "Hello World!", (400,400), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255))
     cv2.imshow("preview", frame)
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('MOG',fgmask)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 32: # exit on ESC
@@ -26,7 +34,8 @@ while rval:
         grayimage = color.rgb2gray(imageresized)*255
         image = noise_remover.Gaussian_filter_forImg(grayimage,3)
         binimage = segmenter.backgroundSubtract(image)
-        
+        input_x = process_img(binimage)
+        #put input_x into trained nl or something else
         cv2.imshow("preview",imageresized)
     if key == 27:
         break
